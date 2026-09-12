83cb3e1308e97f1f71e74710c9ca70d710f5b211
yu <90242233+Yu-20001@users.noreply.github.com>
2026-09-13 00:09:04 +0800

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    bool found = false;
+    void search(vector<vector<char>>& board, int i, int j, string& word, int cnt){
+        if(cnt == word.size()){
+            found = true;
+            return;
+        }
+        if( i >= board.size() || i < 0 || j >= board[0].size() || j < 0 || board[i][j] != word[cnt]) return;
+        char temp = board[i][j];
+        board[i][j] = '#';
+        search(board, i + 1, j, word, cnt + 1);
+        search(board, i - 1, j, word, cnt + 1);
+        search(board, i, j + 1, word, cnt + 1);
+        search(board, i, j - 1, word, cnt + 1);
+        board[i][j] = temp;
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[0].size(); j++){
+                search(board, i, j, word, 0);
+            }
+        }
+        return found;
+    }
+};
