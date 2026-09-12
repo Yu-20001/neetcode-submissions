aee96f4ce77dc3e58c64ae50c041da447bbd46f8
yu <90242233+Yu-20001@users.noreply.github.com>
2026-09-12 15:06:51 +0800

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    vector<vector<int>> ans;
+    void sub(int i, vector<int>& curr, vector<int>& nums){
+        ans.push_back(curr);
+        for(int j = i; j < nums.size(); j++){
+            if(j > i && nums[j] == nums[j-1]) continue;
+            curr.push_back(nums[j]);
+            sub(j + 1, curr, nums);
+            curr.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> curr;
+        sort(nums.begin(), nums.end());
+        sub(0, curr, nums);
+        return ans;
+    }
+};
