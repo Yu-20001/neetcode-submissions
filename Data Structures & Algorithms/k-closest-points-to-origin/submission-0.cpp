2fd71cf2934c9753e62a1877e1a980e89c7319d9
yu <90242233+Yu-20001@users.noreply.github.com>
2026-09-14 23:11:05 +0800

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, int>> pq;
+        unordered_map<float, int> hashMap;
+        vector<vector<int>> ans;
+        for(int i = 0; i < points.size(); i++){
+            int x = points[i][0];
+            int y = points[i][1];
+            int dist = pow(x, 2) + pow(y, 2);
+            pq.push({dist, i});
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        while(!pq.empty()){
+            int idx = pq.top().second;
+            ans.push_back(points[idx]);
+            pq.pop();
+        }
+        return ans;
+    }
+};
